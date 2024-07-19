1db624191e29826cafd10ee918ebb84bf01d83c5
BLeaves <chlorophyll2020@sjtu.edu.cn>
2024-07-19 15:46:11 +0800

    Complete getUserProfile

--- user-service/src/main/java/com/unigpt/user/controller/UserController.java
+++ user-service/src/main/java/com/unigpt/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.unigpt.user.controller;
 
+import com.unigpt.user.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,8 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/internal/users")
 public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
     @GetMapping("/{userid}")
     public ResponseEntity<Object> getUserProfile(@PathVariable Integer userid) {
-        return null;
+        try {
+            return ResponseEntity.ok(service.findUserById(userid));
+        } catch (Exception e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
     }
+
+
 }
