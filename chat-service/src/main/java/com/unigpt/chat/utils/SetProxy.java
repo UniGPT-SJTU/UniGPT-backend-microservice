945c32b1aee43bbed0fb6450b8093e2df545fa17
creeper <creeperhjt@sjtu.edu.cn>
2024-07-23 20:43:02 +0800

    chore: 配置了proxy和环境变量

---
+++ chat-service/src/main/java/com/unigpt/chat/utils/SetProxy.java
@@ -0,0 +1,15 @@
+package com.unigpt.chat.utils;
+
+public class SetProxy {
+    public static void setProxy() {
+        String proxyPort = System.getenv("HTTP_PROXY_PORT");
+        proxyPort = proxyPort == null ? "7890" : proxyPort;
+        String proxyHost = System.getenv("HTTP_PROXY_HOST");
+        proxyHost = proxyHost == null ? "127.0.0.1" : proxyHost;
+        
+        System.setProperty("http.proxyHost", proxyHost);
+        System.setProperty("http.proxyPort", proxyPort);
+        System.setProperty("https.proxyHost", proxyHost);
+        System.setProperty("https.proxyPort", proxyPort);
+    }
+}
