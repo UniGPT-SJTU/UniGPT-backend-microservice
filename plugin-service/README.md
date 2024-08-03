76d64d329d0e7e7dae9c5f11b6e876722a979f7a
creeper <creeperhjt@sjtu.edu.cn>
2024-08-03 22:40:45 +0800

    chore: 修改docker配置,加入User, Plugin

--- plugin-service/README.md
+++ plugin-service/README.md
@@ -4,6 +4,10 @@
 [//]: # (TODO)
 
 ```bash
+export DB_URL="jdbc:mysql://localhost:3309/unigpt_plugin"
+export DB_USERNAME="unigpt"
+export DB_PASSWORD="unigpt"
+
 export USER_SERVICE_URL="http://localhost:8082"
 export BOT_SERVICE_URL="http://localhost:8083"
 export CHAT_SERVICE_URL="http://localhost:8084"
--- plugin-service/src/main/java/com/unigpt/plugin/Repository/UserRepository.java
+++ plugin-service/src/main/java/com/unigpt/plugin/Repository/UserRepository.java
@@ -8,12 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-<<<<<<< HEAD:user-service/src/main/java/com/unigpt/user/repository/UserRepository.java
 //    public Optional<User> findByName(String name);
-    public Optional<User> findByAccount(String account);
-}
-=======
     Optional<User> findByTrueId(Integer userid);
     Optional<User> findByName(String name);
 }
->>>>>>> plugin-service/develop:plugin-service/src/main/java/com/unigpt/plugin/Repository/UserRepository.java
