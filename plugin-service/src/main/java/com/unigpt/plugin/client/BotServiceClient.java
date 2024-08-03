89f012dd767fb245118909cdbc1f2d6c1f7fa6fa
BLeaves <chlorophyll2020@sjtu.edu.cn>
2024-08-03 19:51:45 +0800

    Set BOT_SERVICE_URL

--- plugin-service/src/main/java/com/unigpt/plugin/client/BotServiceClient.java
+++ plugin-service/src/main/java/com/unigpt/plugin/client/BotServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "bot-service", url = "${service.bot-service.url}/internal")
+@FeignClient(name = "bot-service", url = "${services.bot-service.url}/internal")
 public interface BotServiceClient {
     @PostMapping("/plugin/{pluginId}")
     ResponseEntity<Object> createPlugin(
